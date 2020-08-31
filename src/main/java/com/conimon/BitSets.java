592a0f1b45b0a9d998c60214ed81541a47ae3d41
herta <caoherta@gmail.com>
2020-08-31 10:02:49 +0200

    add comments moved toString and toLong methods to EOF

--- src/main/java/com/conimon/BitSets.java
+++ src/main/java/com/conimon/BitSets.java
@@ -10,8 +10,9 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 /**
- * This class provides a representation of BitSets. It contains a BitSet but enhanced it with an length parameter.
- * This allows for bit truncation which is otherwise not possible.
+ * This class provides a representation of a sequence of bits. It contains a BitSet but enhances it with a length parameter.
+ * If not specified the length will be set to the size of the contained BitSet which is 64.
+ * The setup of this class allows for bit truncation that is otherwise not possible with the given BitSet class.
  * The truncation algorithm is designed according to the paper "Lossless Compression of High-volume Numerical Data from Simulations"
  * https://www.researchgate.net/publication/2389424_Lossless_Compression_of_High-volume_Numerical_Data_from_Simulations
  * @author herta
@@ -32,6 +33,11 @@ public class BitSets {
         this.length = bits.size();
     }
 
+    /**
+     * Generates BitSets from a given long
+     * @param value given long values
+     * @return BitSets of this value
+     */
     public static BitSets fromLong(long value) {
         if(value == 0) {
           return new BitSets(new BitSet());
@@ -39,41 +45,21 @@ public class BitSets {
         return new BitSets(BitSet.valueOf(new long[]{value}));
     }
 
-    public Long toLong() {
-        /**
-         * Converts bits to long
-         */
-        return toLong(bits);
-    }
-
-    public static Long toLong(BitSet bs) {
-        /**
-         * Converts bs to long
-         */
-        if (bs.isEmpty()) {
-            return (long) 0;
-        }
-        return bs.toLongArray()[0];
-    }
-
-    public String toString() {
-        return toString(bits, length);
-    }
-
-    public static String toString(BitSet bs) {
-        return toString(bs, bs.size());
-    }
-
-    private static String toString(BitSet bs, int length) {
-        final StringBuilder buffer = new StringBuilder(64);
-        IntStream.range(0, length).mapToObj(i -> bs.get(i) ? '1' : '0').forEach(i -> buffer.insert(0, i));
-        return buffer.toString();
-    }
-
+    /**
+     * Wrapper for truncate method. Truncates BitSets instance by removing leading 0 or 1.
+     * For details of the algorithm please refer to the paper in the {@link BitSets})
+     * @return truncated BitSets
+     */
     public BitSets truncate() {
         return truncate(bits);
     }
 
+    /**
+     * Truncates given BitSet by removing leading 0 or 1 and creates a new BitSets of it.
+     * For details of the algorithm please refer to the paper in the {@link BitSets})
+     * @param bs given BitSet
+     * @return truncated BitSets
+     */
     public static BitSets truncate(BitSet bs) {
         int bitLength;
         // negative
@@ -102,6 +88,11 @@ public class BitSets {
         return bitSets;
     }
 
+    /**
+     * Concatenates a List of BitSets into one BitSet
+     * @param bitSetsList given List of BitSets
+     * @return concatenated BitSet
+     */
     public static BitSet concatenate(List<BitSets> bitSetsList) {
         int totalLength = bitSetsList.stream().mapToInt(BitSets::getLength).sum();
         BitSet bs = new BitSet(totalLength);
@@ -229,5 +220,54 @@ public class BitSets {
          }
 
          return bs;
+    }
+
+    /**
+     * Converts bits to long
+     * @return TODO
+     */
+    public Long toLong() {
+        return toLong(bits);
+    }
+
+    /**
+     * Converts bs to long
+     * @param bs TODO
+     * @return TODO
+     */
+    public static Long toLong(BitSet bs) {
+        if (bs.isEmpty()) {
+            return (long) 0;
+        }
+        return bs.toLongArray()[0];
+    }
+
+    /**
+     * Wrapper for toString methods. Creates a String from the BitSets instance
+     * @return String representation of BitSets instance
+     */
+    public String toString() {
+        return toString(bits, length);
+    }
+
+    /**
+     * Creates String from a given BitSet
+     * @param bs given BitSet
+     * @return String representation of BitSet
+     */
+    public static String toString(BitSet bs) {
+        return toString(bs, bs.size());
+    }
+
+    /**
+     * Creates String from a given BitSet of a specific length. Includes the lowest significant bit to the specified length
+     * @param bs given BitSet
+     * @param length desired length
+     * @return String representation of BitSet
+     */
+    private static String toString(BitSet bs, int length) {
+        final StringBuilder buffer = new StringBuilder(BYTES_OF_LONG * BITS_OF_BYTE);
+        IntStream.range(0, length).mapToObj(i -> bs.get(i) ? '1' : '0').forEach(i -> buffer.insert(0, i));
+        return buffer.toString();
     }
 }
