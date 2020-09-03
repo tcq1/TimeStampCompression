0ead9f1690305c89270753d5fd2ca010831ba400
herta <caoherta@gmail.com>
2020-09-03 02:37:28 +0200

    fixed mili nano bug add prinouts for bugfixing

--- src/main/java/com/conimon/Main.java
+++ src/main/java/com/conimon/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        long nowInNanoSeconds = Instant.now().toEpochMilli() * 1000;
+        long nowInNanoSeconds = Instant.now().toEpochMilli();
         System.out.println("Now in nanoseconds: " + nowInNanoSeconds);
         TimeStampGeneration timeStampGeneration = new TimeStampGeneration(nowInNanoSeconds, 30, 96, 100);
 
@@ -19,6 +19,16 @@ public class Main {
         double compressedSize = compressed.size();
         double uncompressedSize = timeStampGeneration.getTimeStamps().size() * 64;
 
+        for(int i = 0; i<=timeStamps.size(); i++) {
+            if(!timeStamps.get(i).equals(decompressed.get(i))) {
+                System.out.println("i: " + i);
+                System.out.println("original: " + timeStamps.get(i));
+                System.out.println("decomp: " + decompressed.get(i));
+                break;
+            }
+        }
+        System.out.println(timeStamps.size());
+        System.out.println(decompressed.size());
         System.out.println("Timestamps: " + String.format("%s, %s, %s, ..., %s",
                 timeStamps.get(0), timeStamps.get(1), timeStamps.get(2), timeStamps.get(timeStamps.size()-1)));
         System.out.println("Decompressed == timeStamps? " + decompressed.equals(timeStamps));
