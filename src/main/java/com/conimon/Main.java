5fa6a58fcfdd395644379370a4f832db055f360d
herta <caoherta@gmail.com>
2020-09-03 10:18:03 +0200

    main uses now as starttime in nanoseconds

--- src/main/java/com/conimon/Main.java
+++ src/main/java/com/conimon/Main.java
@@ -1,20 +1,28 @@
+
 package com.conimon;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.BitSet;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        TimeStampGeneration timeStamps = new TimeStampGeneration(0, 30, 96, 100);
+        long nowInNanoSeconds = Instant.now().toEpochMilli() * 1000000;
+        System.out.println("Now in nanoseconds: " + nowInNanoSeconds);
+        TimeStampGeneration timeStampGeneration = new TimeStampGeneration(nowInNanoSeconds, 30, 96, 100);
 
         int differenceDegree = 2;
-        BitSet compressed = Compression.compress(timeStamps.getTimeStamps(), differenceDegree);
+        List<Long> timeStamps = timeStampGeneration.getTimeStamps();
+        BitSet compressed = Compression.compress(timeStamps, differenceDegree);
         List<Long> decompressed = Decompression.decompress(compressed, differenceDegree);
 
         double compressedSize = compressed.size();
-        double uncompressedSize = timeStamps.getTimeStamps().size() * 64;
+        double uncompressedSize = timeStampGeneration.getTimeStamps().size() * 64;
 
-        System.out.println("Decompressed == timeStamps? " + decompressed.equals(timeStamps.getTimeStamps()));
+        System.out.println("Timestamps: " + String.format("%s, %s, %s, ..., %s",
+                timeStamps.get(0), timeStamps.get(1), timeStamps.get(2), timeStamps.get(timeStamps.size()-1)));
+        System.out.println("Decompressed == timeStamps? " + decompressed.equals(timeStamps));
         System.out.println("Size of BitSet: " + compressedSize);
         System.out.println("Size of timestamps: " + uncompressedSize);
         System.out.println("Compression rate: " + uncompressedSize / compressedSize);
